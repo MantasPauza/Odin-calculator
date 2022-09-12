ba603652d2dfaa4e77dd33b465df8f8881b3124d
Mantas <mantaspauza@gmail.com>
2022-09-12 21:31:05 +0300

    feat: :sparkles: calculator works

--- script.js
+++ script.js
@@ -1,19 +1,101 @@
-let num1 = 0;
-let num2 = 0;
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalButton = document.querySelector("[data-equal]");
+const deleteButton = document.querySelector("[data-delete]");
+const clearButton = document.querySelector("[data-clear]");
+const prevOper = document.querySelector(".prev_oper");
+const currentOper = document.querySelector(".current_oper");
 
-function addNumbers(num1, num2) {
-    return num1 + num2;
-};
+class Calculator {
+  constructor(prevOper, currentOper) {
+    this.prevOper = prevOper;
+    this.currentOper = currentOper;
+    this.clear();
+  }
+  clear() {
+    this.currentOperand = '';
+    this.prevOperand = '';
+    this.operation = undefined;
+  }
 
-function subtractNumbers(num1, num2) {
-    return num1 - num2;
-};
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    console.log(this.currentOperand);
+  }
 
-function multiplyNumbers(num1, num2) {
-    return num1 * num2;
-};
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return;
+    this.currentOperand += number;
+  }
 
-function divideNumbers(num1, num2) {
-    return num1 / num2;
-};
+  appendOperation(operation) {
+    if (this.currentOperand === '') return;
+    if (this.prevOperand !== '') {
+        this.operate()
+    }
+    this.operation = operation;
+    this.prevOperand = this.currentOperand;
+    this.currentOperand = '';
 
+  }
+
+  operate() {
+    let result 
+    const prev = parseFloat(this.prevOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    if (this.operation === '+') {
+        result = current + prev;
+    } else if (this.operation === '-') {
+        result = current - prev;
+        console.log(result);
+    } else if (this.operation === '*') {
+        result = current * prev;
+    } else if (this.operation === '/') {
+        result = current / prev;
+    } else return;
+    this.currentOperand = result;
+    this.operation = undefined;
+    this.prevOperand = '';
+  }
+  updateScreen() {
+    this.currentOper.innerText = this.currentOperand;
+    if (this.operation != null) {
+        this.prevOper.innerText = `${this.prevOperand} ${this.operation}`
+    } else {
+        this.prevOper.innerText = '';
+    }
+  }
+}
+
+const calculator = new Calculator(prevOper, currentOper);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateScreen();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendOperation(button.innerText);
+    calculator.updateScreen();
+  });
+});
+
+equalButton.addEventListener("click", button => {
+    calculator.operate();
+    calculator.updateScreen();
+    
+});
+
+clearButton.addEventListener("click", button => {
+    calculator.clear();
+    calculator.updateScreen();
+});
+
+deleteButton.addEventListener("click", button => {
+    calculator.delete();
+    calculator.updateScreen();
+});
