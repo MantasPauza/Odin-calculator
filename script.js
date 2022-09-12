6466593159523fb81d7778b2ef2ba915f9fcb3f2
Mantas <mantaspauza@gmail.com>
2022-09-12 19:09:00 +0300

    feat: :art: add html and css
    added html and some css of calculator

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Time to script!
+let num1 = 0;
+let num2 = 0;
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+};
+
+function subtractNumbers(num1, num2) {
+    return num1 - num2;
+};
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+};
+
+function divideNumbers(num1, num2) {
+    return num1 / num2;
+};
+
